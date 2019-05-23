6e988fdaeb31a0f6abf9cc3b0c9371764586f601
StoneWorker <newdaynewlook@126.com>
2019-05-23 20:11:33 +0800

    First time update.

---
+++ WebApi/WebApi/Controllers/UsersController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class UsersController : ApiController
+    {
+       private List<User> users = new List<User>
+        {
+            new User{id=1,name="Jim123",password="123456",selectedDate="",selectedPlace="",voted=false },
+            new User{id=2,name="Jim",password="123456",selectedDate="",selectedPlace="",voted=false },
+            new User{id=3,name="Lily",password="123456",selectedDate="",selectedPlace="",voted=false },
+        };
+
+        // GET: api/Users
+        public IEnumerable<User> Get()
+        {
+            return users;
+        }
+
+        // GET: api/Users/5
+        public User Get(int id)
+        {
+            return users.Find(user => user.id == id);
+        }
+
+        // GET: api/Users/?name=Jim
+        public User Get(string name)
+        {
+            return users.Find(user => user.name == name);
+        }
+
+
+        // POST: api/Users
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Users/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Users/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebApi/WebApi/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.Models
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string password { get; set; }
+        public string selectedDate { get; set; }
+        public string selectedPlace { get; set; }
+        public bool voted { get; set; }
+    }
+}
