867a82e886d5f5ae74bb5ea064d78014588aac1d
StoneWorker <newdaynewlook@126.com>
2019-05-27 22:26:14 +0800

    Read users DB.

--- WebApi/WebApi/Controllers/UsersController.cs
+++ WebApi/WebApi/Controllers/UsersController.cs
@@ -5,6 +5,9 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using WebApi.Models;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace WebApi.Controllers
 {
@@ -16,17 +19,30 @@ namespace WebApi.Controllers
             new User{id=2,name="Jim",   password="e10adc3949ba59abbe56e057f20f883e",selectedDate="",selectedPlace="",voted=false },
             new User{id=3,name="Lily",  password="e10adc3949ba59abbe56e057f20f883e",selectedDate="",selectedPlace="",voted=false },
         };
-         
+
+
+        static string ConnecttionStr = ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString;
+        SqlDataAdapter Myadapter;
+        DataSet MyDataSet;
+
         // GET: api/Users
-        public IEnumerable<User> Get()
+        public DataTable Get()
         {
-            return users;
+            string SelectCmdText = "select * from users";
+            Myadapter = new SqlDataAdapter(SelectCmdText, ConnecttionStr);
+            MyDataSet = new DataSet();
+            Myadapter.Fill(MyDataSet, "users");
+            return (MyDataSet.Tables["users"]);
         }
 
-        // GET: api/Users/5
-        public User Get(int id)
+        // GET: api/Users/2
+        public DataRow Get(int id)
         {
-            return users.Find(user => user.id == id);
+            string SelectCmdText = String.Format( "select * from users where id={0}",id );
+            Myadapter = new SqlDataAdapter(SelectCmdText, ConnecttionStr);
+            MyDataSet = new DataSet();
+            Myadapter.Fill(MyDataSet, "users");
+            return MyDataSet.Tables["users"].Rows[0];
         }
 
         // GET: api/Users/?name=Jim
